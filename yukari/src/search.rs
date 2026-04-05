dbf31b438a4986600fb71732b4bbda88bbddca8b
KazApps <kazuki_shogi@outlook.jp>
2026-04-05 12:23:55 +0900

    double ext bench: 915899

--- yukari/src/search.rs
+++ yukari/src/search.rs
@@ -492,6 +492,10 @@ impl Thread {
                     // The TT move seems uniquely good; extend.
                     if score < singular_beta {
                         extension += 1;
+
+                        if !expected_pvnode && score < singular_beta - 50 {
+                            extension += 1;
+                        }
                     }
                 }
             }
